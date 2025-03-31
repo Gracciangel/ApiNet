c78e3fb99a2b4159b4310734381b3ce4cff9a699
angel Mugracci <angelgracci@outlook.com>
2025-03-31 20:47:39 -0300

    first commit

---
+++ Controller/UserController.cs
@@ -0,0 +1,76 @@
+﻿using DAL.Interfaces;
+using MDL.Dto;
+using MDL.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiModular.Controller
+{
+    [ApiController]
+    [Route("/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        DAL.Interfaces.IUsers _users;
+        public UserController(IUsers users)
+        {
+            _users = users;
+        }
+
+        [HttpGet("/Users/All")]
+        public async Task<ActionResult> Get()
+        {
+            try
+            {
+                var result = await _users.GetAll();
+                if (result == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    return Ok(result);
+                }
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        //crear un usuario 
+        [HttpPost("user/create")]
+        public async Task<ActionResult> CreateUser(CreateDto usuario)
+        {
+            var result = await _users.CreateUser(usuario);
+            if (result == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
+
+        [HttpPost("user/inicio_sesion")]
+        public async Task<ActionResult> InicisarSesion( string data)
+        {
+            var response = await _users.SesionInit(data);
+            try
+            {
+                if (response == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    return Ok(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"{ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using DAL.Contexto;
+using DAL.Interfaces;
+using DAL.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+var contection = builder.Configuration.GetConnectionString("connectionString");
+builder.Services.AddDbContext<ContextDB>(
+    context => context.UseSqlServer(contection));
+
+builder.Services.AddScoped<IUsers, UserServices>(); 
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AlowAll",
+        builder => builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        ); 
+       
+}); 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AlowAll");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
